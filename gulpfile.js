71cfd536a97945c7b375c48ab6dca207af020402
baifang.xing <baifang.xing@qunar.com>
2018-05-24 21:41:04 +0800

    update *.md

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,25 @@ function gitAdd(event) {
 }
 
 gulp.task('add', () => {
+    console.info('add');
     git.add();
 });
 
 gulp.task('commit', () => {
+    console.info('commit');
     return gulp.src(WATCH_PATTERN)
                 .pipe(git.commit('update *.md'));
 });
 
 gulp.task('push', () => {
+    console.info('push');
     git.push('origin', 'master', (err) => {
         if (err) throw err;
     });
 });
 
 gulp.task('status', () => {
+    console.info('status');
     git.status({args: '--porcelain'}, function (err, stdout) {
         if (err) throw err;
     });
@@ -31,7 +35,7 @@ gulp.task('status', () => {
 
 gulp.task('default', function() {
     // 将你的默认的任务代码放在这
-    const watcher = gulp.watch(WATCH_PATTERN, ['status', 'add', 'status', 'commit', 'push']);
+    const watcher = gulp.watch(WATCH_PATTERN, ['add', 'status', 'commit', 'push']);
     watcher.on('change', gitAdd);
 });
 
