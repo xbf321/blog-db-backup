f0a068427a356ca355aa75a5e4dc7b0354a8807c
baifang.xing <baifang.xing@qunar.com>
2018-05-24 21:35:11 +0800

    update *.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
 const gulp = require('gulp');
+const git = require('gulp-git');
+
+
+const WATCH_PATTERN = './*.md';
+function gitAdd(event) {
+    // gulp.src(event.file)
+    console.info(event);
+}
+
+gulp.task('add', () => {
+    git.add();
+});
+
+gulp.task('commit', () => {
+    return gulp.src(WATCH_PATTERN)
+                .pipe(git.commit('update *.md'));
+});
+
+gulp.task('push', () => {
+    git.push('origin', (err) => {
+        if (err) throw err;
+    });
+});
+
+gulp.task('status', () => {
+    git.status({args: '--porcelain'}, function (err, stdout) {
+        if (err) throw err;
+    });
+});
 
 gulp.task('default', function() {
     // 将你的默认的任务代码放在这
-    const watcher = gulp.watch('*.md');
-    watcher.on('change', (event) => {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...', event);
-    });
+    const watcher = gulp.watch(WATCH_PATTERN, ['status', 'add', 'status', 'commit', 'push']);
+    watcher.on('change', gitAdd);
 });
+
+// gulp.task('release', function(callback) {
+//     runSequence('', function(error) {
+//         if (error) {
+//             console.log(error.message);
+//         } else {
+//         console.log('RELEASE FINISHED SUCCESSFULLY');
+//         }
+//         callback(error);
+//     });
+// });
